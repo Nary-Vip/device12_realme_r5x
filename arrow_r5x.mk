dc20141cd4cff7d9ef9645946918acce9fcde1b2
Nary-Vip <nary2vip@gmail.com>
2022-02-17 01:29:41 +0000

    r5x: Update FP to Redfin February

--- arrow_r5x.mk
+++ arrow_r5x.mk
@@ -42,6 +42,6 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys
